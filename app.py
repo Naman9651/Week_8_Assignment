83fca9ffaab1eac560b3283712a675b463cc7cf9
Naman9651 <109916277+Naman9651@users.noreply.github.com>
2022-07-24 23:09:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ string = "Find largest among 3 given numbers"
 st.set_page_config(page_title=string, page_icon="🔍")
 
 st.title('Find largest among 3 given numbers')
-a = st.number_input('Enter first number ')
-b = st.number_input('Enter Second number ')
-c = st.number_input('Enter Third number ')
+a = int(st.number_input('Enter first number '))
+b = int(st.number_input('Enter Second number '))
+c = int(st.number_input('Enter Third number '))
 
   if a > b and a > c:
     largest = a:st.write("A is larger no")
- if b > a and b > c:
+ 
+  if b > a and b > c:
     largest = b:st.write("B is larger no")
- if c > a and c > b:
-    largest = c:st.write("B is larger no")
+ 
+   if c > a and c > b:
+    largest = c:st.write("C is larger no")
     
